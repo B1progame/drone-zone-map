66aa232426e3eb4dcf7c2a0a40173b1b237423e1
B1progame <195572463+B1progame@users.noreply.github.com>
2026-07-15 19:51:30 +0200

    chore: exclude generated build metadata

--- vite.config.js
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({ base: process.env.GITHUB_ACTIONS ? '/drone-zone-map/' : '/', plugins: [react()] });
